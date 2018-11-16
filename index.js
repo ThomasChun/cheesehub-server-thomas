99561fde6d5447edf31c04a0819380396ab042ac
Thomas Chun <tchun2055@gmail.com>
2018-11-16 11:27:26 -0800

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
+const cheeseRouter = require('./routes/cheeses');
 
 const app = express();
 
@@ -21,6 +22,8 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.use('/api/cheeses', cheeseRouter);
 
 function runServer(port = PORT) {
   const server = app
